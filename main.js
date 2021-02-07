22ed69a4c02060b9ed1ea27fa4377e1678922212
Asante Wiebers <Asantewiebers@gmail.com>
2021-02-07 23:39:23 +0100

    main.js added

---
+++ main.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+origin: 'bottom',
+distance: '80px',
+duration: 2000,
+reset: true
+});
+
+
+
+ScrollReveal().reveal('.nav-item', { delay: 500 });
+ScrollReveal().reveal('.big-text', { delay: 2000 });
+ScrollReveal().reveal('#gallery', { delay: 1000 });
